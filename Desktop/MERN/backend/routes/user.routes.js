3f51326182db46a7ef8557e9d68a7e99df290191
chokpapema-beep <chokpapema@gmail.com>
2025-11-26 01:13:00 -0800

    Initial commit

--- Desktop/MERN/backend/routes/user.routes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { createUserController } from "../controller/user.controller.js";
-
-export const userRouter = Router();
-
-userRouter.post('/create', createUserController)
